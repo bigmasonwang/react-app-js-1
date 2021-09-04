362c3371e2a0abf2bf647775b3f987dbd1ceff38
Meisen Wang <bigmasonwang@gmail.com>
2021-09-04 14:36:59 +0930

    test prettier

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 1. 创建 create-react-app 项目, 提交 github
 2. screens 文件夹: 大型组件, components 文件夹: 小组件
 3. 创建 jsconfig.json 文件, 设置绝对路径
-4.
+4. 添加 prettier
+5. https://github.com/okonet/lint-staged
---
+++ src/screens/index.jsx
@@ -0,0 +1,12 @@
+import Entry from "components/Entry";
+import React from "react";
+
+const index = () => {
+  return (
+    <div>
+      <Entry />
+    </div>
+  );
+};
+
+export default index;
